df4e2ba4aac171381a56727a72dc7762dbf8a795
Piotr Wójcik <chocimier@tlen.pl>
2019-10-02 19:31:32 +0200

    Tools

---
+++ tools/json_to_html.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import collections
+import json
+import re
+import sys
+from email.header import decode_header as email_decode_header
+from email.utils import parsedate_to_datetime
+
+from genshi.template import TemplateLoader
+
+posts = json.loads(open(sys.argv[1], 'rb').read())
+loader = TemplateLoader(['.', 'tools'])
+template = loader.load('thread.genshi')
+MAIL_FOOTER = re.compile(r'\s+--\s+You are receiving this because.+\s+' +
+    r'(View it on GitHub:\s+' +
+    r'https://github\.com/voidlinux/[-a-z]+/\w+/\d+\S+\s*$|'
+    r'Reply to this email directly or view it on GitHub:\s+' +
+    r'https://github\.com/voidlinux/[-a-z]+/\w+/\d+\S+\s*$)')
+threads = collections.defaultdict(list)
+
+def thread_id(post):
+    return ((post.get('References') or post['Message-ID'])
+        .partition('/')[-1]
+        .partition('@')[0]
+        .replace('/', ':')
+        )
+
+def decode_header(header):
+    def to_str(x):
+        try:
+            return x[0].decode(x[1] or 'ascii')
+        except AttributeError:
+            return x[0]
+    return ''.join(to_str(i) for i in email_decode_header(header))
+
+for post in posts:
+    post['From'] = decode_header(post['From']).partition('<')[0].strip()
+    post['body'] = MAIL_FOOTER.sub('', post['body'])
+    threads[thread_id(post)].append(post)
+
+for posts in threads.values():
+    posts.sort(key=lambda x: parsedate_to_datetime(x['Date']))
+    print(template
+            .generate(posts=posts)
+            .render('xhtml')
+            , file=open('{}.html'.format(thread_id(posts[0])), 'w'))
+
+print(loader
+        .load('index.genshi')
+        .generate(threads=threads)
+        .render('xhtml')
+        , file=open('index.html', 'w'))
---
+++ tools/mail_to_json.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import json
+import mailbox
+import sys
+
+HEADERS = (
+    'Date',
+    'From',
+    'Message-ID',
+    'References',
+    'Subject',
+)
+try:
+    path = sys.argv[1]
+except IndexError:
+    print('pass path to mbox or maildir as first argument')
+    exit(1)
+
+try:
+    box = mailbox.mbox(path, None, False)
+except IsADirectoryError:
+    box = mailbox.Maildir(path, None, False)
+
+box.lock()
+
+try:
+    result = []
+    for message in box:
+        post = {}
+        if message['Subject'] and '[voidlinux/' in message['Subject']:
+            for header in HEADERS:
+                if header in message:
+                    post[header] = message[header]
+            post['body'] = message.get_payload(0).get_payload()
+            result.append(post)
+    print(json.dumps(result, indent=0))
+finally:
+    box.unlock()
